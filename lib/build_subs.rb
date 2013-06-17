cb0dbd059306a32500cde561d1c6d6c776e1ee12
Jonathan Kovarik <kovarik@nsidc.org>
2013-06-17 16:15:37 -0600

    Added config directory to rakefile

--- lib/build_subs.rb
+++ lib/build_subs.rb
@@ -44,7 +44,7 @@ end
 
 def create_tarball(args, env)
   version_id = generate_version_id
-  sh "tar -cvzf #{env[:repo_dir]}/nsidc_solr_search#{version_id}.tar solr solr-4.3.0/contrib solr-4.3.0/dist solr-4.3.0/example Rakefile Gemfile* lib tasks init nsidc_oai_iso.xslt .ruby-version"
+  sh "tar -cvzf #{env[:repo_dir]}/nsidc_solr_search#{version_id}.tar solr solr-4.3.0/contrib solr-4.3.0/dist solr-4.3.0/example Rakefile Gemfile* lib tasks init nsidc_oai_iso.xslt config"
 end
 
 def configure_collection(collection, target, environment )
