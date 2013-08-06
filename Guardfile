b6a2c08ff16812db6a2ae6d7df65ad0a4ad40a3d
Stuart Reed <stuart.reed@colorado.edu>
2013-08-06 14:56:56 -0600

    Moved fixtures to proper location

--- Guardfile
+++ Guardfile
@@ -1,6 +1,6 @@
-guard :rspec, spec_paths: ['spec/lib'], keep_failed: true, all_after_pass: true, all_on_start: true do
+guard :rspec, spec_paths: ['spec/unit'], keep_failed: true, all_after_pass: true, all_on_start: true do
   watch(/^spec\/.+_spec\.rb/)
-  watch(/^lib\/(.+)\.rb/) { |m| "spec/lib/#{m[1]}_spec.rb" }
+  watch(/^lib\/(.+)\.rb/) { |m| "spec/unit/#{m[1]}_spec.rb" }
 end
 
 guard :rubocop do
--- spec/unit/selectors/ade_nmi_to_solr_spec.rb
+++ spec/unit/selectors/ade_nmi_to_solr_spec.rb
@@ -6,35 +6,45 @@ describe 'NMI ISO to Solr converter' do
   iso_to_solr = IsoToSolr.new(:nmi)
   solr_doc = iso_to_solr.translate fixture
 
-  it 'should grab the correct title' do
-    solr_doc.xpath("/doc/field[@name='title']").text.should be ==
-    'ECMWF deterministic model forecast'
-  end
+  test_expectations = [
+    {
+      title: 'should grab the correct title',
+      xpath: "/doc/field[@name='title']",
+      expected_text: 'ECMWF deterministic model forecast'
+    },
+    {
+      title: 'should grab the correct title',
+      xpath: "/doc/field[@name='summary']",
+      expected_text: "Products from the ECMWF Atmospheric Deterministic medium-range weather forecasts up to ten\ndays. " +
+      "Check out http://www.ecmwf.int/ for details. The model output has been subsetted, reprojected\nand " +
+      'reformatted using FIMEX (http://wiki.met.no/fimex/).'
+    },
+    {
+      title: 'should grab the correct data center',
+      xpath: "/doc/field[@name='data_centers']",
+      expected_text: 'Norwegian Meteorological Institute'
+    },
+    {
+      title: 'should grab the correct get data link',
+      xpath: "/doc/field[@name='dataset_url']",
+      expected_text: 'http://thredds.met.no/thredds/catalog/data/met.no/ecmwf/'
+    },
+    {
+      title: 'should grab the correct updated date',
+      xpath: "/doc/field[@name='last_revision_date']",
+      expected_text: '2009-11-11T00:00:00Z'
+    },
+    {
+      title: 'should grab the correct source',
+      xpath: "/doc/field[@name='source']",
+      expected_text: 'ADE'
+    }
+  ]
 
-  it 'should grab the correct summary' do
-    solr_doc.xpath("/doc/field[@name='summary']").text.should be ==
-    "\nProducts from the ECMWF Atmospheric Deterministic medium-range weather forecasts up to ten\ndays. " +
-    "Check out http://www.ecmwf.int/ for details. The model output has been subsetted, reprojected\nand " +
-    "reformatted using FIMEX (http://wiki.met.no/fimex/). \n"
-  end
-
-  it 'should grab the correct data center' do
-    solr_doc.xpath("/doc/field[@name='data_centers']").text.should be == 'Norwegian Meteorological Institute'
-  end
-
-  it 'should grab the correct get data link' do
-    solr_doc.xpath("/doc/field[@name='dataset_url']").text.should be ==
-    'http://thredds.met.no/thredds/catalog/data/met.no/ecmwf/'
-  end
-
-  it 'should grab the correct updated date' do
-    solr_doc.xpath("/doc/field[@name='last_revision_date']").text.should be ==
-    '2009-11-11T00:00:00Z'
-  end
-
-  it 'should grab the correct source' do
-    solr_doc.xpath("/doc/field[@name='source']").text.should be ==
-    'ADE'
+  test_expectations.each do |expectation|
+    it expectation[:title] do
+      solr_doc.xpath(expectation[:xpath]).text.strip.should eql expectation[:expected_text]
+    end
   end
 
 end
