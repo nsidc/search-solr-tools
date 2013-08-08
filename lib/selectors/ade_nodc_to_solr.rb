c093eca9f66e811a7a2be8573e0462bcb53955c3
Ian Truslove <ian.truslove@nsidc.org>
2013-08-08 12:06:53 -0600

    90% of the NODC to Solr translation done.  Awaiting a fix on the namespace conflicts, then need to uncomment the commented-out tests.

--- lib/selectors/ade_nodc_to_solr.rb
+++ lib/selectors/ade_nodc_to_solr.rb
@@ -1,3 +1,5 @@
+require './lib/selectors/iso_to_solr_format'
+
 # The hash contains keys that should map to the fields in the solr schema, the keys are called selectors
 # and are in charge of selecting the nodes from the ISO document, applying the default value if none of the
 # xpaths resolved to a value and formatting the field.
@@ -5,56 +7,65 @@
 
 NODC = {
   authoritative_id: {
-      xpaths: [''],
-      multivalue: false
-    },
+    xpaths: ['.//gmd:fileIdentifier/gco:CharacterString'],
+    multivalue: false
+  },
   title: {
-      xpaths: [''],
-      multivalue: false
-    },
+    xpaths: ['.//gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/gmd:CI_Citation/gmd:title/gco:CharacterString'],
+    multivalue: false
+  },
   summary: {
-      xpaths: [''],
-      multivalue: false
-    },
+    xpaths: ['.//gmd:identificationInfo/gmd:MD_DataIdentification/gmd:abstract/gco:CharacterString'],
+    multivalue: false
+  },
   data_centers: {
-      xpaths: [''],
-      multivalue: false
-    },
+    xpaths: ['//gmd:distributionInfo/gmd:MD_Distribution/gmd:distributor/gmd:MD_Distributor/gmd:distributorContact/gmd:CI_ResponsibleParty/gmd:organisationName/gco:CharacterString'],
+    multivalue: false
+  },
   authors: {
-      xpaths: [''],
-      multivalue: true
-    },
+    xpaths: ["//gmd:CI_ResponsibleParty[./gmd:role/gmd:CI_RoleCode[@codeListValue='principalInvestigator']]/gmd:individualName/gco:CharacterString"],
+    multivalue: true
+  },
   keywords: {
-      xpaths: [''],
-      multivalue: true
-    },
-  topics: {
-      xpaths: [''],
-      multivalue: true
-    },
-  parameters: {
-      xpaths: [''],
-      multivalue: false
-    },
-  platforms: {
-      xpaths: [''],
-      multivalue: false
-    },
-  sensors: {
-      xpaths: [''],
-      multivalue: true
-    },
+    xpaths: ['//gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword/gco:CharacterString',
+             '//gmd:descriptiveKeywords/gmd:MD_Keywords/gmd:keyword/gmx:Anchor'],
+    multivalue: true
+  },
   last_revision_date: {
-      xpaths: [''],
-      multivalue: false
-    },
+    xpaths: ['.//gmd:dateStamp/gco:Date'],
+    multivalue: false
+  },
   dataset_url: {
-      xpaths: [''],
-      multivalue: false
-    },
+    xpaths: ['.//gmd:MD_DigitalTransferOptions/gmd:onLine/gmd:CI_OnlineResource[./gmd:protocol/gco:CharacterString/text()="ftp"]/gmd:linkage/gmd:URL'],
+    multivalue: false
+  },
+  spatial_coverages: {
+      xpaths: ['.//gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox'],
+      multivalue: true,
+      format: IsoToSolrFormat::SPATIAL_DISPLAY
+  },
+  spatial: {
+      xpaths: ['.//gmd:identificationInfo/gmd:MD_DataIdentification/gmd:extent/gmd:EX_Extent/gmd:geographicElement/gmd:EX_GeographicBoundingBox'],
+      multivalue: true,
+      format: IsoToSolrFormat::SPATIAL_INDEX
+  },
+  temporal_coverages: {
+    xpaths: ['//gmd:EX_TemporalExtent'],
+    multivalue: true,
+    format: proc { |node| IsoToSolrFormat.temporal_display_str node }
+  },
+  temporal: {
+    xpaths: ['//gmd:EX_TemporalExtent'],
+    multivalue: true,
+    format: proc { |node| IsoToSolrFormat.temporal_index_str node }
+  },
+  sensors: {
+    xpaths: ['//gmi:acquisitionInformation/gmi:MI_AcquisitionInformation/gmi:instrument/gmi:MI_Instrument/gmi:citation/gmd:CI_Citation/gmd:title/gco:CharacterString'],
+    multivalue: true
+  },
   source: {
-      xpaths: [''],
-      default_value: ['ADE'],
-      multivalue: false
-    },
+    xpaths: [''],
+    default_values: ['ADE'],
+    multivalue: false
+  },
 }
---
+++ spec/unit/selectors/ade_nodc_to_solr_spec.rb
@@ -0,0 +1,94 @@
+require 'iso_to_solr'
+
+describe 'NODC ISO to Solr converter' do
+
+  puts "\n\n-----------\n"
+  fixture = Nokogiri.XML File.open('spec/unit/fixtures/nodc_iso.xml')
+  iso_to_solr = IsoToSolr.new(:nodc)
+  solr_doc = iso_to_solr.translate fixture
+
+  test_expectations = [
+    {
+      title: 'should grab the correct authoritative id',
+      xpath: "/doc/field[@name='authoritative_id']",
+      expected_text: 'AVHRR_Pathfinder-NODC-L3C-v5.2'
+    },
+    {
+      title: 'should grab the correct title',
+      xpath: "/doc/field[@name='title']",
+      expected_text: 'AVHRR Pathfinder Version 5.2 Level 3 Collated (L3C) Global 4km Sea Surface Temperature for 1981-2011'
+    },
+    {
+      title: 'should grab the correct summary',
+      xpath: "/doc/field[@name='summary']",
+      expected_text: 'The AVHRR Pathfinder Version 5.2 Sea Surface Temperature data set (PFV52) is a collection of global, [snip]'
+    },
+    {
+      title: 'should grab the correct data_centers',
+      xpath: "/doc/field[@name='data_centers']",
+      expected_text: 'DOC/NOAA/NODC > National Oceanographic Data Center, NOAA, U.S. Department of Commerce'
+    },
+    {
+      title: 'should include the correct authors',
+      xpath: "/doc/field[@name='authors'][1]",
+      expected_text: 'Robert Evans'
+    },
+    {
+      title: 'should include the correct keywords',
+      xpath: "/doc/field[@name='keywords'][1]",
+      expected_text: 'Geographic Region > Global Ocean'
+    },
+    {
+      title: 'should grab the correct first sensor',
+      xpath: "/doc/field[@name='sensors'][1]",
+      expected_text: 'Advanced Very High Resolution Radiometer - AVHRR'
+    },
+    {
+      title: 'should grab the correct dataset_url link',
+      xpath: "/doc/field[@name='dataset_url']",
+      # NOTE: I'm grabbing the FTP link explicitly.  NODC has really good data
+      # access links with other representations available.
+      expected_text: 'ftp://ftp.nodc.noaa.gov/pub/data.nodc/pathfinder/Version5.2/'
+    },
+    {
+      title: 'should grab the correct updated date',
+      xpath: "/doc/field[@name='last_revision_date']",
+      expected_text: '2013-07-15'
+    },
+    {
+      title: 'should grab the correct spatial display bounds',
+      xpath: "/doc/field[@name='spatial_coverages']",
+      expected_text: '-180,-90,180,90'
+    },
+    {
+      title: 'should grab the correct spatial bounds',
+      xpath: "/doc/field[@name='spatial']",
+      expected_text: '-180 -90 180 90'
+    },
+## TODO: Deal with the namespace conflicts and uncomment these.
+## I think the code implemented should make the tests pass,
+## but don't be surprised if there are some problems.
+#    {
+#      title: 'should grab the correct temporal coverage',
+#      xpath: "/doc/field[@name='temporal_coverages']",
+#      expected_text: '1981-08-24:00:00Z,2011-12-31T00:00:00Z'
+#    },
+#    {
+#      title: 'should grab the correct temporal range',
+#      xpath: "/doc/field[@name='temporal']",
+#      expected_text: '19.810824 20.111231'
+#    },
+    {
+      title: 'should grab the correct source',
+      xpath: "/doc/field[@name='source']",
+      expected_text: 'ADE'
+    }
+  ]
+
+  test_expectations.each do |expectation|
+    it expectation[:title] do
+      solr_doc.xpath(expectation[:xpath]).text.strip.should eql expectation[:expected_text]
+    end
+  end
+  puts "\n\n==============\n"
+end
