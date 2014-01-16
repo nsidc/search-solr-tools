48543a5bb27f29219891f7717f9b3956a9ca77c3
Michael Brandt <michael.j.brandt@colorado.edu>
2014-01-16 10:59:23 -0700

    fix the duration in the unit test

--- spec/unit/selectors/nsidc_iso_to_solr_spec.rb
+++ spec/unit/selectors/nsidc_iso_to_solr_spec.rb
@@ -119,7 +119,7 @@ describe 'NSIDC ISO to SOLR converter' do
     {
       title: 'should grab the correct temporal duration',
       xpath: "/doc/field[@name='temporal_duration']",
-      expected_text: '12890'
+      expected_text: '12891'
     },
     {
       title: 'should include the first correct temporal values',
