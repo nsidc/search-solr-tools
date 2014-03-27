69f21b14f6c07c127c3fd01436edf339c14e63f7
dharper <dharper@nsidc.org>
2014-03-27 16:34:26 -0600

    #711 - Resolved another merge conflict

--- lib/nsidc_json_to_solr.rb
+++ lib/nsidc_json_to_solr.rb
@@ -33,7 +33,7 @@ class NsidcJsonToSolr
       'dataset_url' => json_doc['datasetUrl'],
       'distribution_formats' => json_doc['distributionFormats'],
       'facet_format' => ((json_doc['distributionFormats'].size == 0) ? ['Not specified'] : json_doc['distributionFormats']),
-      'source' => %w(NSIDC, ADE),
+      'source' => %w(NSIDC ADE),
       'popularity' => json_doc['popularity'],
       'facet_sponsored_program' => translate_internal_data_centers_to_facet_sponsored_program(json_doc['internalDataCenters'])
     )
