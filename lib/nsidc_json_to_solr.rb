85a0d8b26c2574dcee6c34ce32c0566c8bbeab79
Jonathan Kovarik <kovarik@nsidc.org>
2014-03-27 16:19:46 -0600

    #714 -- Add default value to facet_format

--- lib/nsidc_json_to_solr.rb
+++ lib/nsidc_json_to_solr.rb
@@ -37,7 +37,7 @@ class NsidcJsonToSolr
       'last_revision_date' => (IsoToSolrFormat::STRING_DATE.call json_doc['lastRevisionDate']),
       'dataset_url' => json_doc['datasetUrl'],
       'distribution_formats' => json_doc['distributionFormats'],
-      'facet_format' => json_doc['distributionFormats'],
+      'facet_format' => ((json_doc['distributionFormats'].size == 0) ? ['Not specified'] : json_doc['distributionFormats']),
       # task 714 end
       # task 715 start
       'source' => %w(NSIDC ADE),
