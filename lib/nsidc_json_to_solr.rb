417a97890f2ac2745de08f6f6bf01c3ff148724c
Jonathan Kovarik <kovarik@nsidc.org>
2014-04-03 10:12:31 -0600

    #713 - Added new mappings, fixed temporal duration facet/duration values to utilize only the longest duration.

--- lib/nsidc_json_to_solr.rb
+++ lib/nsidc_json_to_solr.rb
@@ -1,3 +1,4 @@
+# rubocop:disable ClassLength
 require './lib/selectors/iso_to_solr_format'
 
 # Translates NSIDC JSON format to Solr JSON add format
@@ -8,6 +9,7 @@ class NsidcJsonToSolr
 # rubocop:disable MethodLength
   def translate(json_doc)
     copy_keys = %w(title summary keywords brokered)
+    temporal_values = generate_temporal_values(json_doc['temporalCoverages'])
     solr_add_hash = json_doc.select { |k, v| copy_keys.include?(k) }
     solr_add_hash.merge!(
       'authoritative_id' => json_doc['authoritativeId'],
@@ -27,7 +29,10 @@ class NsidcJsonToSolr
 
       # task 712 end
       # task 713 start
-
+      'temporal_coverages' => temporal_values['temporal_coverages'],
+      'temporal_duration' => temporal_values['temporal_duration'],
+      'temporal' => temporal_values['temporal'],
+      'facet_temporal_duration' => temporal_values['facet_temporal_duration'],
       # task 713 end
       'last_revision_date' => (IsoToSolrFormat::STRING_DATE.call json_doc['lastRevisionDate']),
       'dataset_url' => json_doc['datasetUrl'],
@@ -38,7 +43,58 @@ class NsidcJsonToSolr
       'facet_sponsored_program' => translate_internal_data_centers_to_facet_sponsored_program(json_doc['internalDataCenters'])
     )
   end
+
+  def generate_temporal_values(temporal_coverages_json)
+    temporal_coverages = []
+    temporal = []
+    max_temporal_duration = nil
+    temporal_coverages_json.each do |coverage|
+      start_time = DateTime.parse(coverage['start']) unless coverage['start'].to_s.empty?
+      end_time = DateTime.parse(coverage['end']) unless coverage['end'].to_s.empty?
+      temporal_duration = generate_temporal_duration_value start_time, end_time
+      time_strings = generate_temporal_time_strings start_time, end_time
+      max_temporal_duration = compare_temporal_duration(max_temporal_duration, temporal_duration)
+      temporal_coverages << time_strings['start_date'] + ', ' + time_strings['end_date']
+      temporal << time_strings['start_integer'] + ' ' + time_strings['end_integer']
+    end unless temporal_coverages_json.nil?
+    facet = generate_facet_temporal_value(max_temporal_duration)
+    { 'temporal_coverages' => temporal_coverages, 'temporal_duration' => max_temporal_duration, 'temporal' => temporal, 'facet_temporal_duration' => facet  }
+  end
+
 # rubocop:enable MethodLength
+
+  def compare_temporal_duration(max_temporal_duration, temporal_duration)
+    if max_temporal_duration.nil? || max_temporal_duration < temporal_duration
+      max_temporal_duration = temporal_duration
+    end unless temporal_duration.nil?
+    max_temporal_duration
+  end
+
+  def generate_temporal_duration_value(start_datetime, end_datetime)
+    start_datetime.nil? ? (return nil) : start_time = start_datetime
+    end_datetime.nil? ?  end_time = DateTime.now : end_time = end_datetime
+    Integer(end_time - start_time).abs + 1
+  end
+
+  def generate_temporal_time_strings(start_time, end_time)
+    time_strings = {}
+    if start_time.nil?
+      time_strings.merge!('start_date' => '', 'start_integer' => DateTime.parse('00010101').strftime('%C.%y%m%d'))
+    else
+      time_strings.merge!('start_date' => start_time.strftime('%Y-%m-%d'), 'start_integer' => start_time.strftime('%C.%y%m%d'))
+    end
+    if end_time.nil?
+      time_strings.merge!('end_date' => '', 'end_integer' => DateTime.now.strftime('%C.%y%m%d'))
+    else
+      time_strings.merge!('end_date' => end_time.strftime('%Y-%m-%d'), 'end_integer' => end_time.strftime('%C.%y%m%d'))
+    end
+  end
+
+  def generate_facet_temporal_value(duration)
+    return ['No Temporal Information'] if duration.nil?
+    years = duration.to_i / 365
+    IsoToSolrFormat.temporal_duration_range(years)
+  end
 
   def translate_iso_topic_categories(iso_topic_categories_json)
     iso_topic_categories_json.map { |t| t['name'] } unless iso_topic_categories_json.nil?
@@ -114,3 +170,4 @@ class NsidcJsonToSolr
     parts
   end
 end
+# rubocop:disable ClassLength
--- spec/unit/nsidc_json_to_solr_spec.rb
+++ spec/unit/nsidc_json_to_solr_spec.rb
@@ -5,6 +5,45 @@ describe NsidcJsonToSolr do
     @translator = described_class.new
   end
 
+  it 'generates temporal values from NSIDC JSON' do
+    temporal_coverages_json = [{ 'start' => '1986-12-14T00:00:00-07:00', 'end' => '1992-11-13T00:00:00-07:00' },
+                               { 'start' => '', 'end' => '1992-01-18T00:00:00-07:00' }]
+    temporal_values = @translator.generate_temporal_values(temporal_coverages_json)
+    temporal_values['temporal_coverages'][0].should eql('1986-12-14, 1992-11-13')
+    temporal_values['temporal_duration'].should eql 2162
+    temporal_values['temporal'][0].should eql '19.861214 19.921113'
+    temporal_values['facet_temporal_values'].should eql ['1+ years', '5+ years']
+  end
+
+  it 'generates temporal value defaults when there are none present in NSIDC JSON' do
+    temporal_values = @translator.generate_temporal_values([])
+    temporal_values['temporal_coverages'].should eql []
+    temporal_values['temporal_duration'].should eql nil
+    temporal_values['temporal'].should eql []
+    temporal_values['facet_temporal_values'].should eql ['No Temporal Information']
+  end
+
+  it 'generates a temporal duration value based on the longest single temporal coverage' do
+    temporal_coverages_json = [{ 'start' => '1956-01-01T00:00:00-07:00', 'end' => '1964-01-01T00:00:00-07:00' },
+                               { 'start' => '1994-01-01T00:00:00-07:00', 'end' => '1996-01-01T00:00:00-07:00' }]
+    temporal_values = @translator.generate_temporal_values(temporal_coverages_json)
+    temporal_values['temporal_duration'].should eql 2923
+    temporal_values['facet_temporal_values'].should eql ['1+ years', '5+ years']
+  end
+
+  it 'generates correct start values when no start date is specified' do
+    temporal_coverages_json = [{ 'start' => '', 'end' => '1992-01-01T00:00:00-07:00' }]
+    temporal_values = @translator.generate_temporal_values(temporal_coverages_json)
+    temporal_values['temporal_coverages'].should eql [', 1992-01-01']
+    temporal_values['temporal_duration'].should eql nil
+    temporal_values['temporal'].should eql ['00.010101 19.920101']
+    temporal_values['facet_temporal_values'].should eql ['No Temporal Information']
+  end
+
+  it 'generates a temporal duration value' do
+    @translator.generate_temporal_duration_value(DateTime.parse('1956-01-01T00:00:00-07:00'), DateTime.parse('1958-01-01T00:00:00-07:00')).should eql 732
+  end
+
   it 'translates NSIDC JSON date to SOLR format iso8601 date' do
     date = '2013-03-12T21:18:12-06:00'
     (IsoToSolrFormat::STRING_DATE.call date).should eql '2013-03-12T21:18:12Z'
