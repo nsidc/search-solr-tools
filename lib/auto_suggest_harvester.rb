11fe2ffb72a58fd1580fceafd6ffd8b57e7489aa
billinb <billinb@nsidc.org>
2014-06-26 19:22:19 -0600

    #881 - Adding weight to schema and harvest

--- lib/auto_suggest_harvester.rb
+++ lib/auto_suggest_harvester.rb
@@ -10,16 +10,18 @@ class AutoSuggestHarvester < HarvesterBase
     @env_settings = SolrEnvironments[@environment]
   end
 
-  def short_full_split_add_creator(value, count)
+  def short_full_split_add_creator(value, count, field_weight)
     add_docs = []
     value.split(' > ').each do |v|
-      add_docs.concat(standard_add_creator(v, count))
+      add_docs.concat(standard_add_creator(v, count, field_weight))
     end
     add_docs
   end
 
-  def standard_add_creator(value, count)
-    [{ 'add' => { 'doc' => { 'text_suggest' => value, 'source' => 'NSIDC' } } }]
+  def standard_add_creator(value, count, field_weight)
+    count_weight = count <= 1 ? 0.4 : Math.log(count)
+    weight = field_weight * count_weight
+    [{ 'add' => { 'doc' => { 'text_suggest' => value, 'source' => 'NSIDC', 'weight' => weight } } }]
   end
 
   def fetch_auto_suggest_facet_data(url, fields)
@@ -35,7 +37,7 @@ class AutoSuggestHarvester < HarvesterBase
     add_docs = []
     facet_response['facet_counts']['facet_fields'].each do |facet_name, facet_values|
       facet_values.each_slice(2).to_a.each do |facet_value|
-        new_docs = fields[facet_name][:creator].call(facet_value[0], facet_value[1])
+        new_docs = fields[facet_name][:creator].call(facet_value[0], facet_value[1], fields[facet_name][:weight])
         add_docs.concat(new_docs)
       end
     end
@@ -48,10 +50,10 @@ class AutoSuggestHarvester < HarvesterBase
 
   def nsidc_fields
     { 'authoritative_id' => { weight: 1, creator: method(:standard_add_creator) },
-      'full_title' => { weight: 1, creator: method(:standard_add_creator) },
-      'copy_parameters' => { weight: 1, creator: method(:standard_add_creator) },
-      'full_platforms' => { weight: 1, creator: method(:short_full_split_add_creator) },
-      'full_sensors' => { weight: 1, creator: method(:short_full_split_add_creator) },
+      'full_title' => { weight: 2, creator: method(:standard_add_creator) },
+      'copy_parameters' => { weight: 5, creator: method(:standard_add_creator) },
+      'full_platforms' => { weight: 3, creator: method(:short_full_split_add_creator) },
+      'full_sensors' => { weight: 3, creator: method(:short_full_split_add_creator) },
       'full_authors' => { weight: 1, creator: method(:standard_add_creator) } }
   end
 
