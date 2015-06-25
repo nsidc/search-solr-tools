74a2dcee633f3d5e96a8f253278ad239fd8025fa
Jonathan Kovarik <kovarik@nsidc.org>
2015-06-25 05:34:57 -0600

    Add harvest_and_delete method
    Added harvest_and_delete method to the eol harvester
    Added unit test coverage for harvest_eol_into_solr

--- lib/search_solr_tools/harvesters/eol.rb
+++ lib/search_solr_tools/harvesters/eol.rb
@@ -10,23 +10,30 @@ module SearchSolrTools
         @translator = SearchSolrTools::Translators::EolToSolr.new
       end
 
+      def harvest_and_delete
+        puts 'Running harvest of EOL catalog using the following configured EOL URLs:'
+        SearchSolrTools::SolrEnvironments[:common][:eol].each { |x| puts x }
+        super(method(:harvest_eol_into_solr), "data_centers:\"#{Helpers::SolrFormat::DATA_CENTER_NAMES[:EOL][:long_name]}\"")
+      end
+
       def harvest_eol_into_solr
-        eol_dataset_urls.each do |dataset|
+        solr_add_queries = eol_dataset_urls.map do |dataset|
           begin
             doc = open_xml_document(dataset)
             if doc.xpath('//xmlns:metadata').size > 1
               # THREDDS allows for a dataset of datasests, EOL should not utilize this
-              fail "Complex dataset encountered at #{data_doc.xpath('//xmlns:catalog').to_html}"
+              fail "Complex dataset encountered at #{doc.xpath('//xmlns:catalog').to_html}"
             end
             metadata_doc = open_xml_document(doc.xpath('//xmlns:metadata')[0]['xlink:href'])
-            insert_doc = [{ 'add' => { 'doc' => @translator.translate(doc, metadata_doc) } }]
-            insert_solr_docs insert_doc, Base::JSON_CONTENT_TYPE
+            { 'add' => { 'doc' => @translator.translate(doc, metadata_doc) } }
           rescue => e
             puts "ERROR: #{e}"
             puts "Failed to translate this record: #{doc} -> #{metadata_doc}"
             raise e if @die_on_failure
+            next
           end
         end
+        insert_solr_docs solr_add_queries, Base::JSON_CONTENT_TYPE
       end
 
       def eol_dataset_urls
@@ -43,11 +50,6 @@ module SearchSolrTools
           config.strict
         end
         doc
-      end
-
-      def harvest_and_delete
-        puts 'Running harvest of EOL endpoints'
-        super(method(:harvest_eol_into_solr), "data_centers:\"#{Helpers::SolrFormat::DATA_CENTER_NAMES[:EOL][:long_name]}\"")
       end
     end
   end
--- spec/unit/harvesters/eol_harvester_spec.rb
+++ spec/unit/harvesters/eol_harvester_spec.rb
@@ -3,6 +3,23 @@ require 'spec_helper'
 describe SearchSolrTools::Harvesters::Eol do
   before :all do
     @harvester = described_class.new
+    @translation = {
+      'title' => 'Buoy: IABP Daily Buoy Positions [Moritz, R.]',
+      'authoritative_id' => 'ucar.ncar.eol.dataset.13_453',
+      'data_centers' => 'UCAR NCAR - Earth Observing Laboratory',
+      'facet_data_center' => 'UCAR NCAR - Earth Observing Laboratory | UCAR NCAR EOL',
+      'summary' => "During SHEBA, the Polar Science Center at the University of Washington deployed a network of automatic
+buoys for monitoring synoptic-scale fields of pressure, temperature, and ice motion throughout the Arctic Basin. This dataset contains the daily buoy positions. For more information, please see the readme file.", 'temporal_coverages' => ['1997-12-31,1998-12-31'], 'temporal_duration' => 365, 'temporal' => ['19.971231 19.981231'], 'facet_temporal_duration' => ['1+ years'], "last_revisi
+on_date" => '2007-11-05T04:12:58Z',
+      'source' => 'ADE',
+      'keywords' => %w(Arctic Surface),
+      'authors' => 'Richard Moritz',
+      'dataset_url' => 'http://data.eol.ucar.edu/codiac/dss/id=13.453',
+      'facet_spatial_coverage' => false, 'facet_spatial_scope' => 'Between 1 and 170 degrees of latitude change | Regional',
+      'spatial_coverages' => '70.0 -170.0 80.0 -130.0',
+      'spatial_area' => 10.0,
+      'spatial' => '-170.0 70.0 -130.0 80.0'
+    }
   end
 
   describe '#eol_dataset_urls' do
@@ -36,7 +53,7 @@ describe SearchSolrTools::Harvesters::Eol do
     end
   end
 
-  describe 'initialize' do
+  describe '#initialize' do
     it 'has a translator instance variable' do
       expect(@harvester.instance_variable_defined?(:@translator)).to eql true
     end
@@ -48,7 +65,43 @@ describe SearchSolrTools::Harvesters::Eol do
   end
 
   describe '#harvest_eol_into_solr' do
-    it 'inserts a translated document' do
+    before :each do
+      @thredds_metadata = Nokogiri::XML(File.open(File.expand_path('../../../unit/fixtures/eol_thredds_106_295.thredds.xml', __FILE__)))
+      @dataset_metadata = Nokogiri::XML(File.open(File.expand_path('../../../unit/fixtures/eol_thredds_106_295.metadata.xml', __FILE__)))
+      allow_any_instance_of(described_class).to receive(:eol_dataset_urls).and_return(['http://test_url.xml'])
+      @harvester = described_class.new('development', true)
+    end
+
+    it 'inserts a translated document' do # TODO: Fixture
+      allow_any_instance_of(SearchSolrTools::Translators::EolToSolr).to receive(:translate).and_return(@translation)
+      allow_any_instance_of(described_class).to receive(:open_xml_document).and_return(Nokogiri::XML(%(
+        <TEST xmlns="TEST">
+          <metadata xlink:href='http://test_dataset.thredds.xml'>
+      )))
+      expect_any_instance_of(SearchSolrTools::Harvesters::Base).to receive(:insert_solr_docs).with(
+        [{ 'add' => { 'doc' => @translation } }], SearchSolrTools::Harvesters::Base::JSON_CONTENT_TYPE
+      ).and_return true
+      @harvester.harvest_eol_into_solr
+    end
+
+    it 'fails when complex metadata is encountered' do
+      allow_any_instance_of(described_class).to receive(:open_xml_document).and_return(Nokogiri::XML(%(
+        <TEST xmlns="TEST">
+          <metadata xlink:href='http://test_dataset.thredds.xml'>
+          <metadata xlink:href='http://test_dataset2.thredds.xml'>
+      )))
+      expect { @harvester.harvest_eol_into_solr }.to raise_error(RuntimeError)
+    end
+
+    it 'continue on failure if die_on_failure is false' do
+      @harvester = described_class.new('development', false)
+      allow_any_instance_of(described_class).to receive(:open_xml_document).and_return(Nokogiri::XML(%(
+        <TEST xmlns="TEST">
+          <metadata xlink:href='http://test_dataset.thredds.xml'>
+          <metadata xlink:href='http://test_dataset2.thredds.xml'>
+      )))
+      expect_any_instance_of(SearchSolrTools::Harvesters::Base).to receive(:insert_solr_docs).and_return true
+      @harvester.harvest_eol_into_solr
     end
   end
 end
