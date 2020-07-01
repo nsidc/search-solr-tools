7ede0671e0f355b9eeb65b1ac0df2c5a677efee7
Scott Lewis <scott.lewis@nsidc.org>
2020-07-01 11:38:39 -0600

    Removing require_all gem, doing regular requires instead

--- lib/search_solr_tools.rb
+++ lib/search_solr_tools.rb
@@ -1,8 +1,6 @@
-require 'require_all'
 require_relative './search_solr_tools/config/environments'
 require_relative './search_solr_tools/version'
 
-require_rel './search_solr_tools/helpers'
-require_rel './search_solr_tools/selectors'
-require_rel './search_solr_tools/harvesters'
-require_rel './search_solr_tools/translators'
+%w( helpers selectors harvesters translators ).each do |subdir|
+  Dir[File.join(__dir__, 'search_solr_tools', subdir, '*.rb')].each { |file| require file }
+end
--- lib/search_solr_tools/harvesters/oai.rb
+++ lib/search_solr_tools/harvesters/oai.rb
@@ -1,4 +1,5 @@
 require 'json'
+require_relative './base'
 
 module SearchSolrTools
   module Harvesters
