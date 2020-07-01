775e61323c6fdc1cbe15c6a1cc23df6a2eec7fa4
Scott Lewis <scott.lewis@nsidc.org>
2020-07-01 14:27:07 -0600

    Require base first, selectors last

--- lib/search_solr_tools.rb
+++ lib/search_solr_tools.rb
@@ -1,10 +1,12 @@
 require_relative './search_solr_tools/config/environments'
 require_relative './search_solr_tools/version'
 
+require File.join(__dir__, 'search_solr_tools', 'harvesters', 'base.rb')
 %w( helpers selectors harvesters translators ).each do |subdir|
   puts File.join(__dir__, 'search_solr_tools', subdir)
-  Dir[File.join(__dir__, 'search_solr_tools', subdir, '*.rb')].each do |file|
-    next if subdir == 'helpers' && file.include?('selectors.rb')  #punt
+  Dir[File.join(__dir__, 'search_solr_tools', subdir, '*.rb')]
+      .reject{ |f| f.include? 'selectors.rb'}
+      .each do |file|
     puts file
     require file
   end
