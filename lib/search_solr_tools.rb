9408df22878d3033e86e588f433e2f83ac6a70f4
Scott Lewis <scott.lewis@nsidc.org>
2020-07-01 13:54:17 -0600

    Trying to figure out why circleci fails

--- lib/search_solr_tools.rb
+++ lib/search_solr_tools.rb
@@ -2,5 +2,9 @@ require_relative './search_solr_tools/config/environments'
 require_relative './search_solr_tools/version'
 
 %w( helpers selectors harvesters translators ).each do |subdir|
-  Dir[File.join(__dir__, 'search_solr_tools', subdir, '*.rb')].each { |file| require file }
+  puts File.join(__dir__, 'search_solr_tools', subdir)
+  Dir[File.join(__dir__, 'search_solr_tools', subdir, '*.rb')].each do |file|
+    puts file
+    require file
+  end
 end
