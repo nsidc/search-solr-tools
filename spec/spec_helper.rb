14ad5a2cad60ffefab03ad449fa8ebd21aa11996
Scott Lewis <scott.lewis@colorado.edu>
2022-08-31 11:45:59 -0600

    Updating call to get identifier list

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,11 @@ require 'search_solr_tools/helpers/solr_format'
 require 'search_solr_tools/helpers/harvest_status'
 
 GZIP_DEFLATE_IDENTITY = 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3'
+
+# TODO: This is used to allow use of the `stub_chain` in `harvester_base_spec`.
+# We may consider removing `stub_chain` and using a different approach instead.
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
--- spec/unit/harvesters/nsidc_json_harvester_spec.rb
+++ spec/unit/harvesters/nsidc_json_harvester_spec.rb
@@ -10,7 +10,7 @@ describe SearchSolrTools::Harvesters::NsidcJson do
   end
 
   it 'should retrieve dataset identifiers from the NSIDC OAI url' do
-    stub_request(:get, 'http://integration.nsidc.org/api/dataset/metadata/oai?verb=ListIdentifiers&metadata_prefix=iso&retired=false')
+    stub_request(:get, 'http://integration.nsidc.org/api/dataset/metadata/oai?verb=ListIdentifiers&metadataPrefix=dif&retired=false')
       .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
       .to_return(status: 200, body: File.open('spec/unit/fixtures/nsidc_oai_identifiers.xml'))
 
@@ -19,7 +19,7 @@ describe SearchSolrTools::Harvesters::NsidcJson do
 
   describe 'Adding documents to Solr' do
     it 'constructs a hash with doc children' do
-      stub_request(:get, 'http://integration.nsidc.org/api/dataset/metadata/oai?verb=ListIdentifiers&metadata_prefix=iso&retired=false')
+      stub_request(:get, 'http://integration.nsidc.org/api/dataset/metadata/oai?verb=ListIdentifiers&metadataPrefix=dif&retired=false')
         .with(headers: { 'Accept' => '*/*', 'User-Agent' => 'Ruby' })
         .to_return(status: 200, body: File.open('spec/unit/fixtures/nsidc_oai_identifiers.xml'))
 
@@ -48,7 +48,7 @@ describe SearchSolrTools::Harvesters::NsidcJson do
     end
 
     it 'constructs a sucessful doc children hash and an errors hash for failed ids' do
-      stub_request(:get, 'http://integration.nsidc.org/api/dataset/metadata/oai?verb=ListIdentifiers&metadata_prefix=iso&retired=false')
+      stub_request(:get, 'http://integration.nsidc.org/api/dataset/metadata/oai?verb=ListIdentifiers&metadataPrefix=dif&retired=false')
         .with(headers: { 'Accept' => '*/*' })
         .to_return(status: 200, body: File.open('spec/unit/fixtures/nsidc_oai_identifiers.xml'))
 
