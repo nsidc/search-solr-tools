c062e236d34086c0b769a782ff06df39b9eab5e9
Scott Lewis <scott.lewis@colorado.edu>
2023-09-15 16:02:06 -0600

    Only console logging when running tests

--- lib/search_solr_tools/logging/sst_logger.rb
+++ lib/search_solr_tools/logging/sst_logger.rb
@@ -11,18 +11,21 @@ module SSTLogger
     end
 
     def new_logger
-      @logger = Logging.logger['search_solr_tools logger']
-      # @logger.level = log_level(ENV['SOLR_HARVEST_LOG_LEVEL'])
+      logger = Logging.logger['search_solr_tools']
 
       unless ENV['SOLR_HARVEST_STDOUT_LEVEL'].nil?
         new_stdout = Logging.appenders.stdout
         new_stdout.level = log_level(ENV['SOLR_HARVEST_STDOUT_LEVEL'])
-        @logger.add_appenders new_stdout
+        logger.add_appenders new_stdout
       end
 
-      new_file = Logging.appenders.file(log_file)
-      new_file.level = log_level(ENV['SOLR_HARVEST_LOG_LEVEL'])
-      @logger.add_appenders new_file
+      unless ENV['SOLR_HARVEST_LOG_FILE'] == 'none'
+        new_file = Logging.appenders.file(log_file)
+        new_file.level = log_level(ENV['SOLR_HARVEST_LOG_LEVEL'])
+        logger.add_appenders new_file
+      end
+
+      logger
     end
 
     def log_level(level)
